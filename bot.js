db342dd8b7d63ee15cd4148ba69143a7e675b24a
First Presbyterian Robotics Team <4221fpdrobotics@gmail.com>
2017-05-10 18:24:25 -0400

    initial commit

---
+++ bot.js
@@ -0,0 +1,53 @@
+var SlackBot = require('slackbots');
+//var fs = require('fs');
+var VERS = 'ALPHA';
+
+var todo = '{ "todo": ['+'] }';
+
+var bot = new SlackBot(
+{
+	token: 'xoxb-180908282243-iUheztWH4Mkv2KlM48RzzjeH',
+	name: 'Smida Bot'
+});
+
+bot.on('start', function() { //startup. need to load saved data here, too.
+	
+});
+
+bot.on('close', function() {
+	//fs.writeFile( "todo.json", JSON.stringify( todo ), "utf8");
+});
+
+bot.on('message', function(data) { //message parsing.
+	if(data.type == "message"){
+		console.log(data);
+		if(data.text.includes('@smidabot' @@ data.channel != null){ //for commands
+			args = data.text.replace('@smidabot', '').trim().substring(' ');
+			if(args.length > 1){
+				if(args[0].toLowerCase()=='todo'){
+					if(args.length > 2){
+						
+					}
+				}
+				bot.postMessage(data.channel, 'Valid commands: \ntodo <@assigned_to> <description>'
+		}
+		
+		
+		if(data.text.toLowerCase().includes("hi smidabot") && data.channel != null){
+			console.log("contains greeting");
+			getRealName(data.user).then(function(name){
+				bot.postMessage(data.channel, 'Hey @' + name[0] + ' !');
+			});
+		}
+	}
+});
+
+function getRealName(id){
+	return bot.getUsers().then(function(list){
+		return Promise.all(list.members.map(function (user) {
+			if(user.id == id)
+				return user.name;
+		}));
+	});
+	return "can't obtain first name";
+}
